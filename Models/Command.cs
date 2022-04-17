0123e0f083222b9db429cd98cceb9b38171b078c
konasMajid <kondasmajid@Gmail.com>
2022-04-17 13:41:35 +0000

    Modified Commands Model with DataAnatations

--- Models/Command.cs
+++ Models/Command.cs
@@ -1,9 +1,13 @@
 
- namespace Commander.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace Commander.Models
 {
     public class Command
     {
+            [Key]
             public int Id { get; set; }
+            [Required][MaxLength(100)]
             public string Howto { get; set; }
             public string Line { get; set; }
             public string Platform { get; set; }
