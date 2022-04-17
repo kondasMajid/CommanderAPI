d679ce07bfd7362fad4413e3a599c5426fef62ec
konasMajid <kondasmajid@Gmail.com>
2022-04-17 17:53:27 +0000

    Installed Autp Mapper and configure in Startup

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,9 @@ namespace Commander
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Commander", Version = "v1" });
             });
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
