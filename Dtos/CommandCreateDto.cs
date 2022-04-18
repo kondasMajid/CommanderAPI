dd5d8ef552ae1d68c46fa707db849a9b41dcee98
konasMajid <kondasmajid@Gmail.com>
2022-04-18 15:08:25 +0000

    Implemented DataAnotations for CommandCreadDtos

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,11 +1,16 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
             // public int Id { get; set; }
+              [Required][MaxLength(100)]
             public string Howto { get; set; }
+            [Required]
             public string Line { get; set; }
+            [Required]
             public string Platform { get; set; }
     }
 }
